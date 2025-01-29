d8b0a7b4a12a2a3b87a27bd68db6b58e77147326
rato42 <lucasolt@gmail.com>
2025-01-29 00:02:57 -0300

    1.06a

--- Code/FUNCTION_ShouldMaxAim.lua
+++ Code/FUNCTION_ShouldMaxAim.lua
@@ -11,11 +11,30 @@ function ShouldMaxAim(context) ----- used in AICalcAttacksAndAim
     local er = context.EffectiveRange
     local pb = const.Weapons.PointBlankRange
     local to_check_range = pb
-
     if dist and (dist > to_check_range * const.SlabSizeX) then
-        -- print("----------", context.unit.session_id, " should max aim bcause", target.session_id,
-        --      "is at ", dist / const.SlabSizeX)
         return true
     end
     return false
 end
+
+--[[function GetIdealAimLevels(context) ----- used in AICalcAttacksAndAim
+
+    local target = context.current_target
+    local attack_pos = context.attacker_pos
+    if not target or not attack_pos or not IsKindOf(context.weapon, "Firearm") then
+        return false
+    end
+
+    local dist = context.attacker_pos:Dist(target:GetPos())
+
+    local er = context.EffectiveRange
+    local pb = const.Weapons.PointBlankRange
+    local to_check_range = pb
+
+    if dist and (dist <= pb * const.SlabSizeX) then
+        return 0
+    elseif dist and (dist <= er * const.SlabSizeX) then
+        return -- 1 (ou 2?)
+    end
+    return "Max Aim"
+end]]
